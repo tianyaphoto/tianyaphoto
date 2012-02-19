fb8187d60f7400c03bbc62997c6604563105ea8f
root <root@aliyun.(none)>
2012-02-19 12:17:58 +0800

    init myproject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails32project::Application.config.secret_token = 'f5b6640a441096dcc3ace2569ba49c6272bdd610a497a20ac88dafd2567b0dca88ddcb33dbb46d72902d6b0dbf44f2fc2902e117f68e4df08ad8ab7c2331ce86'
---
+++ test/integration/user_flows_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class UserFlowsTest < ActionDispatch::IntegrationTest
+  test "visit welcome index page" do
+    get "welcome/index"
+    assert_response :success
+  end
+end
