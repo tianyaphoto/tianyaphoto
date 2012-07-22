afb8af28b85b36edabaf2b5d6317673f546a700e
chucai <hexudong08@gmail.com>
2012-07-22 21:35:52 +0800

    增加了分类页面， 增加了分类

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@
 Rails32project::Application.routes.draw do
   
 
+
   devise_for :users
 
   resources :photos
   resources :tags, :only => [:index]
+  resources :category,  :only => [:index, :show]
 
   get "welcome/index"
   get "welcome/form"
