fee987c0b95c592f5a4c5830bfd77acb3c963f3e
chucai <hexudong08@gmail.com>
2012-08-05 19:48:11 +0800

    update

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*- 
+class CategoryController < ApplicationController
+  before_filter :init_categories 
+
+  def index
+    category_id = params[:cat_id] || 0
+  end
+
+  def show
+    id = params[:id]
+    parent = Category.find_by_id id
+    Rails.logger.info "depth is #{parent.depth}"
+    @current = parent
+    if parent.depth == 0
+      @second = parent.children
+      category_ids = @second.collect(&:id)
+    elsif parent.depth == 1
+      @first_parent = parent.parent
+      @second = @first_parent.children
+      @third = parent.children
+      category_ids = @third.collect(&:id)
+    elsif parent.depth == 2
+      @first_parent = parent.root
+      @second = @first_parent.children
+      @third = parent.self_and_siblings
+      category_ids = [@current.id]
+    end
+    @photos = Photo.where("category_id in (?)", category_ids).all 
+    render :index
+  end
+
+  private
+  def init_categories
+    @first = Category.roots
+  end
+
+end
